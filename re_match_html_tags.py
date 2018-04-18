045becc10f4951175ed24fa7dac334ac1d0fd9e2
feng-hui <982698913@qq.com>
2018-04-18 19:51:56 +0800

    the useage of re with \s+

---
+++ re_match_html_tags.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @author = 'Feng_hui'
+# @time = '2018/4/18 19:45'
+# @email = 'fengh@asto-inc.com'
+# 好用推荐-正则表达式中\s+的使用
+# 参考地址：https://docs.python.org/3/library/re.html
+# [直接在页面中搜索\s找到对应的位置有对应的解释]
+import re
+
+
+def remove_special_tags(value):
+    """主要用于爬虫中抓取到的文本处理"""
+    assert isinstance(value, str)
+    return re.sub(r'\s+', '', temp_str)
+
+
+if __name__ == "__main__":
+    temp_str = '上海\n          -虹桥区\n         走着走着就散了'
+    print(remove_special_tags(temp_str))
