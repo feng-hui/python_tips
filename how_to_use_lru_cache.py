4254b536a26c57a4e6ffbc1ea6804d2c257f0675
feng-hui <982698913@qq.com>
2019-06-03 14:09:00 +0800

    how to use lru cache

---
+++ how_to_use_lru_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*- 
+# @author FH
+# @email: capricorn1203@126.com
+# @time: 2019/6/3 13:22
+import time
+
+
+def fib(number: int) -> int:
+    if number == 0:
+        return 0
+    if number == 1:
+        return 1
+    return fib(number - 1) + fib(number - 2)
+
+
+start_time = time.time()
+result = fib(40)
+print(f'Result: {result}')
+print(f'Duration: {time.time() - start_time}s')
